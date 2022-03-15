4a24727f046594bf8c09c77aa5796233d2f22012
primahafiz <primahafiz@gmail.com>
2022-03-15 12:38:50 +0700

    BFS and graph visualizer

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Folder_Crawling
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            BFS mainBFS = new BFS();
+            string[] ans= mainBFS.BFSMain("D:\\Prima\\ITB\\IF sem 4\\RPL", "notulen asistensi.docx", false);
+            BFS.printArr(ans);
+        }
+    }
+}
