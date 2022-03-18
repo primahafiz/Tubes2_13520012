a18894a6704362ebdda1b6b33a7188c7ab10c35e
01 494 Aji <ajiandhika05@gmail.com>
2022-03-18 13:58:43 +0700

    GUI

---
+++ GUI/GUI/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace GUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+			dialog.IsFolderPicker = true;
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				textBox1.Text = dialog.FileName;
+			}
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			String path = textBox1.Text;
+			String file = textBox2.Text;
+			String method;
+			Boolean occurences = checkBox1.Checked;
+			Boolean isChecked = radioButton1.Checked;
+			String parent = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName).FullName).FullName).FullName + @"\pictures\" ;
+
+			if (isChecked)
+				method = radioButton1.Text;
+			else
+				method = radioButton2.Text;
+
+			MessageBox.Show(path + "\n" + file + "\n" + occurences + "\n" + method + "\n" + parent);
+			pictureBox1.Image = Image.FromFile(parent + file);
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
